538eb8ac098d6b283fb9b00330b32f2871886ce5
pablo-osuna-vargas <pabloosunavargas@gmail.com>
2026-07-02 12:37:26 -0300

    pages y practicas 37 y 38 (incompleta)

--- clase-11/practica-37/js/script.js
+++ clase-11/practica-37/js/script.js
@@ -1,5 +1,4 @@
-// Definiciones y capturas //
-const votos = [candidato1, candidato2, candidato3];
+// Definiciones y capturas asi lee al iniciar todo lo que va a usar en el orden correcto //
 let legajos = [];
 let capturaForm = document.querySelector("#legajo");
 let enviar = document.querySelector("#enviar");
@@ -9,9 +8,45 @@ let boton3 = document.querySelector("#candidato3");
 let cierre = document.querySelector("#cierre");
 let resultados = document.querySelector("#resultados");
 
-let voto1 = 0;
-let voto2 = 0;
-let voto3 = 0;
+// array de objetos a ser actualizados en forEach //
+let candidatos = [
+  {
+    candidato: 1,
+    votos: 0,
+    boton: boton1
+  },
+
+  {
+    candidato: 2,
+    votos: 0,
+    boton: boton2
+  },
+
+  {
+    candidato: 3,
+    votos: 0,
+    boton: boton3
+  }
+];
+
+// cada objeto del array es recorrido por la funcion del forEach con sus propiedades como parametros (candidato) // 
+// y su botón correspondiente //
+candidatos.forEach(function(candidato, i) {
+candidato.boton.addEventListener("click", function(e) {
+  e.preventDefault();
+    candidatos[i].votos++;
+    boton1.disabled = true;
+    boton2.disabled = true;
+    boton3.disabled = true;
+    recuento();
+  });
+});
+
+function recuento() {
+  for(i = 0; i < candidatos.length; i++) {
+    console.log(`candidato: ${candidatos[i].candidato}, votos: ${candidatos[i].votos}`);
+  };
+}
 
 // Ingreso de datos y validaciones //
 enviar.addEventListener("click", function(e) {
@@ -21,130 +56,55 @@ enviar.addEventListener("click", function(e) {
   boton3.disabled = false;
 
   let numLegajo = Number(document.querySelector("#legajo").value);
-
   if(numLegajo == "" || isNaN(numLegajo)) {
     alert("por favor ingrese un número válido");
+    return;
   }
 
+  verificarPadron(numLegajo);
   legajos.push(numLegajo);
-
-  verificarPadron();
-
+  document.querySelector("#legajo").value = "";
   console.log(legajos);
 });
 
-boton1.addEventListener("click", function(e) {
-  e.preventDefault();
-  voto1++;
-  boton1.disabled = true;
-  boton2.disabled = true;
-  boton3.disabled = true;
-
-  
-  vaciar();
-  console.log(candidato1);
-});
-
-function verificarPadron() {
+// funciones para validar y vaciar //
+function verificarPadron(numLegajo) {
+  let yaVoto = false;
   for(let i = 0; i < legajos.length; i++) {
-      if(numLegajo === legajos[i]) {
-        alert("ud ya ha votado");
+      if(numLegajo == legajos[i]) {
+        yaVoto = true;
       }
     }
-}
 
-/*
-
-});
-
-boton2.addEventListener("click", function(e) {
-  e.preventDefault();
-  boton2.disabled = false;
-});
-
-boton3.addEventListener("click", function(e) {
-  e.preventDefault();
-  boton3.disabled = false;
-});
-
-function ingresarDatos() {
-  let titulo = document.querySelector("#titulo").value;
-  let autor = document.querySelector("#autor").value;
-  let año = Number(document.querySelector("#año").value);
-  let prestamo = document.querySelector("#prestamo").value;
-  
-    if(titulo == "") {
-      alert("ingresá un título");
-      return;
+    if(yaVoto) {
+      alert("ud ya ha votado");
     }
+  };
 
-    if(autor == "") {
-      alert("ingresá un autor");
-      return;
-    }
-
-    if(isNaN(año) || año <= 0 || año >2026) {
-      alert("ingresá un año válido");
-      return;
-    }
-
-    if(prestamo == "") {
-      alert("por favor indica Si o No");
-      return;
-    }
-
-    let objetoLibro = {
-      titulo: titulo,
-      autor: autor,
-      año: año,
-      prestamo: prestamo,
-    }
-
-    libros.push(objetoLibro);
-
-    console.log(libros);
-
-    vaciar();
-}
-*/
-function vaciar() {
-  document.querySelector("#legajo").value = "";
-}
-
-/*
 // Calcular y mostrar resultados //
-botonCalcular.addEventListener("click", function(e) {
+cierre.addEventListener("click", function(e) {
   e.preventDefault();
   calcular();
 });
 
 function calcular() {
-  let masAntiguo = libros[0];
-  let enPrestamo = 0;
-  let mas100 = 0;
+  let ganador = candidatos[0];
+  let totalVotos = 0;
 
-  for(let i = 0; i < libros.length; i++) {
-    let antiguedad = (2026 - libros[i].año);
-    if(libros[i].año < masAntiguo.año) {
-      masAntiguo = libros[i];
+  for(let i = 0; i < candidatos.length; i++) {
+    if(candidatos[i].votos > ganador.votos) {
+      ganador = candidatos[i];
     }
+    totalVotos += candidatos[i].votos;
+  };
 
-    if(antiguedad > 100) {
-      mas100++;
-    }
-    
-    if(libros[i].prestamo === "si") {
-      enPrestamo++;
-    }
-  }
 
-    let mayores = (mas100 * 100) / libros.length;
+let porcentaje = (ganador.votos* 100) / totalVotos;
 
-  resultados.innerText += 
-`El libro mas antiguo es ${masAntiguo.titulo}, ${masAntiguo.año}
-Hay actualmente ${enPrestamo} libros en préstamo
-${mayores}% de libros tienen mas de 100 años`;
+resultados.innerText += 
+`El candidato ganador es: ${ganador.candidato}, con ${ganador.votos} votos
+con un ${porcentaje}% de votos a favor.
+${totalVotos} personas votaron en este comicio`
 
-  console.log(resultados);
-}
-*/
+  console.log(resultados.innerText);
+};
--- clase-11/practica-38/js/script.js
+++ clase-11/practica-38/js/script.js
@@ -1,5 +1,110 @@
-// Definiciones y capturas //
+// Definiciones y capturas asi lee al iniciar todo lo que va a usar en el orden correcto //
+let legajos = [];
+let capturaForm = document.querySelector("#legajo");
+let enviar = document.querySelector("#enviar");
+let boton1 = document.querySelector("#candidato1");
+let boton2 = document.querySelector("#candidato2");
+let boton3 = document.querySelector("#candidato3");
+let cierre = document.querySelector("#cierre");
+let resultados = document.querySelector("#resultados");
 
-// Ingreso de datos //
+// array de objetos a ser actualizados en forEach //
+let candidatos = [
+  {
+    candidato: 1,
+    votos: 0,
+    boton: boton1
+  },
+
+  {
+    candidato: 2,
+    votos: 0,
+    boton: boton2
+  },
+
+  {
+    candidato: 3,
+    votos: 0,
+    boton: boton3
+  }
+];
+
+// cada objeto del array es recorrido por la funcion del forEach con sus propiedades como parametros (candidato) // 
+// y su botón correspondiente //
+candidatos.forEach(function(candidato, i) {
+candidato.boton.addEventListener("click", function(e) {
+  e.preventDefault();
+    candidatos[i].votos++;
+    boton1.disabled = true;
+    boton2.disabled = true;
+    boton3.disabled = true;
+    recuento();
+  });
+});
+
+function recuento() {
+  for(i = 0; i < candidatos.length; i++) {
+    console.log(`candidato: ${candidatos[i].candidato}, votos: ${candidatos[i].votos}`);
+  };
+}
+
+// Ingreso de datos y validaciones //
+enviar.addEventListener("click", function(e) {
+  e.preventDefault();
+  boton1.disabled = false;
+  boton2.disabled = false;
+  boton3.disabled = false;
+
+  let numLegajo = Number(document.querySelector("#legajo").value);
+  if(numLegajo == "" || isNaN(numLegajo)) {
+    alert("por favor ingrese un número válido");
+    return;
+  }
+
+  verificarPadron(numLegajo);
+  legajos.push(numLegajo);
+  document.querySelector("#legajo").value = "";
+  console.log(legajos);
+});
+
+// funciones para validar y vaciar //
+function verificarPadron(numLegajo) {
+  let yaVoto = false;
+  for(let i = 0; i < legajos.length; i++) {
+      if(numLegajo == legajos[i]) {
+        yaVoto = true;
+      }
+    }
+
+    if(yaVoto) {
+      alert("ud ya ha votado");
+    }
+  };
 
 // Calcular y mostrar resultados //
+cierre.addEventListener("click", function(e) {
+  e.preventDefault();
+  calcular();
+});
+
+function calcular() {
+  let ganador = candidatos[0];
+  let totalVotos = 0;
+
+  for(let i = 0; i < candidatos.length; i++) {
+    if(candidatos[i].votos > ganador.votos) {
+      ganador = candidatos[i];
+    }
+    totalVotos += candidatos[i].votos;
+  };
+
+
+let porcentaje = (ganador.votos* 100) / totalVotos;
+
+resultados.innerText += 
+`El candidato ganador es: ${ganador.candidato}, con ${ganador.votos} votos
+con un ${porcentaje}% de votos a favor.
+${totalVotos} personas votaron en este comicio`
+
+  console.log(resultados.innerText);
+};
