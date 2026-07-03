8e31bf03bb5835ccb9abdbe153277285fc06e982
pablo-osuna-vargas <pabloosunavargas@gmail.com>
2026-07-02 23:09:03 -0300

    practica 38 (incompleta) y practica 39

--- clase-11/practica-38/js/script.js
+++ clase-11/practica-38/js/script.js
@@ -1,110 +1,116 @@
 // Definiciones y capturas asi lee al iniciar todo lo que va a usar en el orden correcto //
-let legajos = [];
-let capturaForm = document.querySelector("#legajo");
-let enviar = document.querySelector("#enviar");
-let boton1 = document.querySelector("#candidato1");
-let boton2 = document.querySelector("#candidato2");
-let boton3 = document.querySelector("#candidato3");
-let cierre = document.querySelector("#cierre");
-let resultados = document.querySelector("#resultados");
+let equipamiento = [];
+let form = document.querySelector("#carga");
+let descripcion = document.querySelector("#descripcion");
+let estado = document.querySelector("#estado");
+let botonCargar = document.querySelector("#cargarEquipo");
+let botonEnviar = document.querySelector("#enviarDatos");
+let pedidos = document.querySelector("#pedidos");
+pedidos.style.display = "none";
+let listaDisponible = document.querySelector("#lista");
+listaDisponible.style.display = "none";
 
-// array de objetos a ser actualizados en forEach //
-let candidatos = [
-  {
-    candidato: 1,
-    votos: 0,
-    boton: boton1
-  },
+let cargaFinalizada = false;
 
-  {
-    candidato: 2,
-    votos: 0,
-    boton: boton2
-  },
-
-  {
-    candidato: 3,
-    votos: 0,
-    boton: boton3
-  }
-];
-
-// cada objeto del array es recorrido por la funcion del forEach con sus propiedades como parametros (candidato) // 
-// y su botón correspondiente //
-candidatos.forEach(function(candidato, i) {
-candidato.boton.addEventListener("click", function(e) {
+let recargar = document.querySelector("#recargar");
+recargar.addEventListener("click", function(e){
   e.preventDefault();
-    candidatos[i].votos++;
-    boton1.disabled = true;
-    boton2.disabled = true;
-    boton3.disabled = true;
-    recuento();
+  location.reload();
+})
+
+// 1er momento: carga de equipos y muestra disponibilidad //
+botonCargar.addEventListener("click", function(e){
+  e.preventDefault();
+
+  let objetoEquipo = {
+    descripcion: document.querySelector("#descripcion").value,
+    estado: document.querySelector("#estado").value,
+  }
+
+  equipamiento.push(objetoEquipo);
+
+  document.querySelector("#descripcion").value = "";
+  document.querySelector("#estado").value = "";
+  console.log(equipamiento);
+})
+
+botonEnviar.addEventListener("click", function(e){
+  e.preventDefault();
+  cargaFinalizada = true;
+  pedidos.disabled = false;
+  pedidos.style.display = "block";
+
+  listaDisponible.disabled = false;
+  listaDisponible.style.display = "block";
+  listaDisponible.innerHTML = ""; // limpio la lista para cargar solo los datos nuevos ingresados //
+    equipamiento.forEach(function(equipamiento){
+    listaDisponible.innerHTML += `<li>${equipamiento.descripcion}, ${equipamiento.estado}</li>`;
+  });
+})
+
+
+// 2do momento: pedido y validacion de disponibilidad //
+let pedidosArray = [];
+let cargarPedido = document.querySelector("#cargarPedido");
+let enviarPedido = document.querySelector("#enviarPedido");
+let prestado = false;
+let cargaFinalizadaPedido = false;
+let mostrarInfo = document.querySelector("#mostrarInfo");
+mostrarInfo.style.display = "none";
+let mas5 = 0;
+
+cargarPedido.addEventListener("click", function(e){
+  e.preventDefault();
+
+  let objetoPedido = {
+      equipoDescripcion: document.querySelector("#equipoDescripcion").value,
+      dias: Number(document.querySelector("#prestamoTiempo").value)
+    }
+
+  pedidosArray.push(objetoPedido);
+
+  document.querySelector("#equipoDescripcion").value = "";
+  document.querySelector("#prestamoTiempo").value = "";
+  console.log(pedidosArray);
+});
+
+enviarPedido.addEventListener("click", function(e){
+  e.preventDefault();
+  cargaFinalizadaPedido = true;
+  mostrarInfo.disabled = false;
+  mostrarInfo.style.display = "block";
+  mostrarInfo.innerHTML = ""; // limpio la lista para cargar solo los datos nuevos ingresados //
+
+// recorro array de pedidos y creo variable para buscar en el array de equipos declarados //
+for(i = 0; i < pedidosArray.length; i++) {
+  let buscado = pedidosArray[i];
+  let encontrado = false;
+
+    if(pedidosArray[i].dias > 5) {
+      mas5++;
+      console.log(mas5);
+    }
+
+    for(j = 0; j < equipamiento.length; j++) {
+    // dos IF anidados para ejecutar una busqueda, y si se encuentra el item actualizar estado de inventario //
+    if(equipamiento[j].descripcion === buscado.equipoDescripcion) {
+      encontrado = true;
+      if(equipamiento[j].estado === "no-prestado") {
+        equipamiento[j].estado = "prestado";
+      }
+      console.log(`${buscado.equipoDescripcion} esta prestado por ${buscado.dias} días`);
+    } 
+
+    if(equipamiento[j].estado = "prestado") {
+      alert("el equipo solicitado se encuentra prestado actualmente");
+      return;
+    } 
+  }
+};
+    
+  pedidosArray.forEach(function(pedidosArray){
+  mostrarInfo.innerHTML += `<li>${pedidosArray.equipoDescripcion}, x ${pedidosArray.dias} días</li>
+  <li>${mas5} equipos fueron pedidos x más de 5 días</li>
+  <li>${pedidosArray[i].equipoDescripcion} fueron prestados`
   });
 });
-
-function recuento() {
-  for(i = 0; i < candidatos.length; i++) {
-    console.log(`candidato: ${candidatos[i].candidato}, votos: ${candidatos[i].votos}`);
-  };
-}
-
-// Ingreso de datos y validaciones //
-enviar.addEventListener("click", function(e) {
-  e.preventDefault();
-  boton1.disabled = false;
-  boton2.disabled = false;
-  boton3.disabled = false;
-
-  let numLegajo = Number(document.querySelector("#legajo").value);
-  if(numLegajo == "" || isNaN(numLegajo)) {
-    alert("por favor ingrese un número válido");
-    return;
-  }
-
-  verificarPadron(numLegajo);
-  legajos.push(numLegajo);
-  document.querySelector("#legajo").value = "";
-  console.log(legajos);
-});
-
-// funciones para validar y vaciar //
-function verificarPadron(numLegajo) {
-  let yaVoto = false;
-  for(let i = 0; i < legajos.length; i++) {
-      if(numLegajo == legajos[i]) {
-        yaVoto = true;
-      }
-    }
-
-    if(yaVoto) {
-      alert("ud ya ha votado");
-    }
-  };
-
-// Calcular y mostrar resultados //
-cierre.addEventListener("click", function(e) {
-  e.preventDefault();
-  calcular();
-});
-
-function calcular() {
-  let ganador = candidatos[0];
-  let totalVotos = 0;
-
-  for(let i = 0; i < candidatos.length; i++) {
-    if(candidatos[i].votos > ganador.votos) {
-      ganador = candidatos[i];
-    }
-    totalVotos += candidatos[i].votos;
-  };
-
-
-let porcentaje = (ganador.votos* 100) / totalVotos;
-
-resultados.innerText += 
-`El candidato ganador es: ${ganador.candidato}, con ${ganador.votos} votos
-con un ${porcentaje}% de votos a favor.
-${totalVotos} personas votaron en este comicio`
-
-  console.log(resultados.innerText);
-};
--- clase-11/practica-39/js/script.js
+++ clase-11/practica-39/js/script.js
@@ -1,5 +1,87 @@
-// Definiciones y capturas //
+let atletas = [];
+let form = document.querySelector("#cargar");
+let mostrarInfo = document.querySelector("#mostrarInfo");
+mostrarInfo.style.display = "none";
+mostrarInfo.disabled = true;
 
-// Ingreso de datos //
+let botonCargarDatos = document.querySelector("#cargarDatos");
+botonCargarDatos.addEventListener("click", function(e){
+  e.preventDefault();
 
-// Calcular y mostrar resultados //
+  let ejerciciosElegidos = [];
+  if(document.querySelector("#fuerza").checked) {
+    ejerciciosElegidos.push(document.querySelector("#fuerza").value)
+  }
+  if(document.querySelector("#flexibilidad").checked) {
+    ejerciciosElegidos.push(document.querySelector("#flexibilidad").value)
+  }
+  if(document.querySelector("#resistencia").checked) {
+    ejerciciosElegidos.push(document.querySelector("#resistencia").value)
+  }
+
+  let objetoAtleta = {
+    nombre: document.querySelector("#nombre").value,
+    edad: Number(document.querySelector("#edad").value),
+    ejercicios: ejerciciosElegidos,
+    minutos: Number(document.querySelector("#minutos").value),
+  }
+
+  atletas.push(objetoAtleta);
+  vaciar();
+})
+
+let botonResumen= document.querySelector("#resumen");
+botonResumen.addEventListener("click", function(e){
+  e.preventDefault();
+  calcular();
+})
+
+let botonRecargar = document.querySelector("#recargar");
+botonRecargar.addEventListener("click", function(e){
+  e.preventDefault();
+  location.reload();
+})
+
+function vaciar() {
+  document.querySelector("#nombre").value = "";
+  document.querySelector("#edad").value = "";
+  document.querySelector("#fuerza").checked = false;
+  document.querySelector("#flexibilidad").checked = false;
+  document.querySelector("#resistencia").checked = false;
+  document.querySelector("#minutos").value = "";
+}
+
+function calcular() {
+  let masEntrenado = atletas[0];
+  let menosEntrenados = [];
+
+  for(let i = 0; i < atletas.length; i++) {
+    if(atletas[i].minutos > masEntrenado.minutos) {
+      masEntrenado = atletas[i];
+      console.log(masEntrenado.nombre);
+    }
+    if(atletas[i].minutos < 60) {
+      let menos60 = atletas[i];
+      menosEntrenados.push(menos60);
+      console.log(menosEntrenados[i].nombre, menosEntrenados[i].minutos);
+    }
+
+    // creo una variable con un string vacío para concatenar un item con los nombres y tiempos del array //
+    let infoAtletas = "";
+    for(let i = 0; i < atletas.length; i++) {
+      infoAtletas += `<li>${atletas[i].nombre} entrenó durante ${atletas[i].minutos} minutos</li>`;
+    }
+
+    let porcentaje = (menosEntrenados.length * 100) / atletas.length;
+    console.log(porcentaje)
+
+    mostrarInfo.disabled = false;
+    mostrarInfo.style.display = "block";
+    mostrarInfo.innerHTML = "";
+    mostrarInfo.innerHTML += `<ul>
+      ${infoAtletas}
+      <li>${masEntrenado.nombre} entrenó mas tiempo que el resto</li>
+      <li>${porcentaje}% entrenaron menos de 60 minutos</li>
+    </ul>`;
+  }
+};
